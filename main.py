46c6b815660523c4ef1748992064d619b2fd2038
Ga1kinSasha <115646309+Ga1kinSasha@users.noreply.github.com>
2023-12-11 17:35:09 +0500

    version 2.0

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ db1_connect = sqlite3.connect('users.db')
 db1_cursor = db1_connect.cursor()
 
 users_list = [
-    (1, 'Turgeneva 4', 'Durka'),
-    (2, 'Lenina 51', 'x2 Durka'),
-    (3, 'Kraulya 73', 'OldHome')
+    (1, 'Turgeneva 4', 'Durka', '88003555311'),
+    (2, 'Lenina 51', 'x2 Durka', '88003555511'),
+    (3, 'Kraulya 73', 'OldHome', '88003555711')
 ]  # get data of users
 
 
@@ -14,22 +14,22 @@ def users_db(data_of_users):
     db1_cursor.execute("""CREATE TABLE IF NOT EXISTS users(
         user_id INTEGER PRIMARY KEY,
         user_login TEXT,
-        user_password TEXT
+        user_password TEXT,
+        user_phone_number TEXT
     )
     """)
-    db1_cursor.executemany("INSERT INTO users (user_login, user_password) VALUES (?, ?);",
-                           [(user[1], user[2]) for user in data_of_users])
+    db1_cursor.executemany("INSERT INTO users VALUES(?,?,?,?);", users_list)
     db1_connect.commit()
 
 
-users_db(users_list)
+# users_db(users_list)
 
 db2_connect = sqlite3.connect('specialists.db')
 db2_cursor = db2_connect.cursor()
 
 specialists_list = [
-    (1, 'Дрочислав Сын Сергея', 'Vodka'),
-    (2, 'Артём', 'Pivo')
+    (1, 'Дрочислав Сын Сергея', 'Vodka', '88003255711'),
+    (2, 'Артём', 'Pivo', '88001553711')
 ]  # get data of specialists
 
 
@@ -37,28 +37,30 @@ def specialists_db(data_of_specialists):
     db2_cursor.execute("""CREATE TABLE IF NOT EXISTS specialists(
         specialists_id INTEGER PRIMARY KEY,
         specialists_login TEXT,
-        specialists_password TEXT
+        specialists_password TEXT,
+        specialists_phone_number INTEGER
     )
     """)
-    db2_cursor.executemany("INSERT INTO specialists VALUES(?,?,?);", data_of_specialists)  # filled in the database
+    db2_cursor.executemany("INSERT INTO specialists VALUES(?,?,?,?);", data_of_specialists)  # filled in the database
     db2_connect.commit()
 
 
-specialists_db(specialists_list)
+# specialists_db(specialists_list)
 
 db3_connect = sqlite3.connect('application.db')
 db3_cursor = db3_connect.cursor()
 
 application_list = [
-    (1, '2004-08-30', 'Turgeneva 4', 'Pending', 'John Doe')
+    (1, 3, '2004-08-30', 'Pending', 'John Doe'),
+    (2, 2, '2002-02-22', 'Pending', 'John Snow')
 ]  # get application of application
 
 
 def application_db(data_of_application):
     db3_cursor.execute("""CREATE TABLE IF NOT EXISTS application(
         application_id INTEGER PRIMARY KEY,
+        user_id INTEGER,
         application_date DATE,
-        application_address TEXT,
         application_status TEXT,
         specialists TEXT
     )
@@ -67,8 +69,7 @@ def application_db(data_of_application):
     db3_connect.commit()
 
 
-
-application_db(application_list)
+# application_db(application_list)
 
 
 def search_db(cursor, table_name):  # return all data from DB in list
@@ -82,3 +83,19 @@ def add_to_table(cursor, connection, table_name, values):  # add value to table_
     query = f"INSERT INTO {table_name} VALUES ({placeholders});"
     cursor.execute(query, values)
     connection.commit()
+
+
+# users_data = search_db(db1_cursor, 'users')
+# print(users_data)
+
+def get_user_by_id(cursor, user_id):
+    cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+    user_data = cursor.fetchone()
+    if user_data:
+        _, user_login, _, user_phone_number = user_data
+        return f"Адрес: {user_login}. Номер телефона: {user_phone_number}."
+    else:
+        return "Пользователь не найден."
+
+# user_data_by_id = get_user_by_id(db1_cursor, 1)
+# print(user_data_by_id)
