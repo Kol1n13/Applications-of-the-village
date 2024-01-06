c4ab13fd8b7c27b15f839363ccb2fd04c911286f
Firchalka <nikorlov26@gmail.com>
2024-01-06 14:11:50 +0500

    В методе user-profile при обработке пост запроса нужно чтобы в таблицу заявок вносилась заявка

--- database.py
+++ database.py
@@ -52,9 +52,12 @@ db3_connect = sqlite3.connect('application.db')
 db3_cursor = db3_connect.cursor()
 
 application_list = [
-    (1, "Turgeneva 4", "Александр", "Сантехника", "Трубы горят", '2004-08-30', 'Pending', 'саня', 1, "88003555311", "88003255711"),
-    (2, "Lenina 51", "Никита", "Сетевое оборудование", "Роутер сломался", '2002-02-22', 'Pending', 'саня', 2, "88003555511" , "88003255711"),
-    (3, "Turgeneva 4", "Александр", "Проблема со здоровьем", "Висячий", "2050-02-11", 'Pending', 'саня', 1, "88003555311", "88003255711")
+    (1, "Turgeneva 4", "Александр", "Сантехника", "Трубы горят", '2004-08-30',
+     'Pending', 'саня', 1, "88003555311", "88003255711"),
+    (2, "Lenina 51", "Никита", "Сетевое оборудование", "Роутер сломался",
+     '2002-02-22', 'Pending', 'саня', 2, "88003555511", "88003255711"),
+    (3, "Turgeneva 4", "Александр", "Проблема со здоровьем", "Висячий",
+     "2050-02-11", 'Pending', 'саня', 1, "88003555311", "88003255711")
 ]  # get application of application
 
 
@@ -73,10 +76,13 @@ def application_db(data_of_application):
         specialist_phone TEXT
     )
     """)
-    db3_cursor.executemany("INSERT INTO application VALUES(?,?,?,?,?,?,?,?,?,?,?);",data_of_application)
+    db3_cursor.executemany(
+        "INSERT INTO application VALUES(?,?,?,?,?,?,?,?,?,?,?);",
+        data_of_application)
     db3_connect.commit()
 
-#application_db(application_list)
+
+# application_db(application_list)
 
 
 def search_users_db():
@@ -86,6 +92,7 @@ def search_users_db():
         all_results = cursor.fetchall()
     return all_results
 
+
 def search_applications_db():
     with sqlite3.connect('application.db') as connection:
         cursor = connection.cursor()
@@ -93,12 +100,14 @@ def search_applications_db():
         all_results = cursor.fetchall()
     return all_results
 
+
 def search_specialists_db():
     with sqlite3.connect('specialists.db') as connection:
         cursor = connection.cursor()
         cursor.execute(f"SELECT * FROM specialists")
         all_results = cursor.fetchall()
     return all_results
+
 
 def get_dictionary_of_specialists():
     data = search_specialists_db()
@@ -123,6 +132,14 @@ def add_to_table(cursor, connection, table_name,
     cursor.execute(query, values)
     connection.commit()
 
+def count_applications():
+    with sqlite3.connect('application.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT COUNT(*) FROM application")
+        count = cursor.fetchone()[0]
+    return count
+
+
 
 # print(get_dictionary_of_users())
 
@@ -130,7 +147,25 @@ def add_to_table(cursor, connection, table_name,
 def get_user_by_id(user_id):
     with sqlite3.connect('application.db') as connection:
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM application WHERE user_id = ?", (user_id,))
+        cursor.execute("SELECT * FROM application WHERE user_id = ?",
+                       (user_id,))
+        user_data = cursor.fetchall()
+        if user_data:
+            return user_data
+        else:
+            return None
+
+def add_application_to_db(values):
+    with sqlite3.connect('application.db') as connection:
+        cursor = connection.cursor()
+        placeholders = ', '.join(['?' for _ in values])
+        query = f"INSERT INTO application VALUES ({placeholders});"
+        cursor.execute(query, values)
+
+def get_true_user_by_id(user_id):
+    with sqlite3.connect('users.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
         user_data = cursor.fetchall()
         if user_data:
             return user_data
